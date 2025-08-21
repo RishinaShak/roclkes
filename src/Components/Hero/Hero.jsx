3b8dc68767cce2b9dc6b0e8a95fafc2f4854e992
RishinaShak <rishina49@gmail.com>
2025-08-21 11:28:21 +0545

    updated text in Hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,12 +6,13 @@ const Hero = () => {
   return (
     <div className='container'>
         <div className="left-section">
-              <p>Experience crystal </p>
+              <p>Experience crystal and smooth </p>
               <h1>clear sound</h1>
               <p>with our top rated speakers</p>
             <button className='btn'>SHOP NOW</button>
             <div className='box'>
                   <span>50% OFF</span> on Rs.25000
+
             </div>
         </div>
         <div className="right-section">
