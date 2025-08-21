242d35f000335b12167a0fe24d25457fa106f3d3
RishinaShak <rishina49@gmail.com>
2025-08-21 16:58:35 +0545

    Time Created

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
 import './Hero.css';
 import model from '../../assets/model1.png'
+import moment from "moment";
 
 const Hero = () => {
+
+//  const now = moment();
+//  console.log(now,"time")
+
   return (
     <div className='container'>
         <div className="left-section">
-              <p>Experience crystal and smooth </p>
+              <p>Experience crystal </p>
               <h1>clear sound</h1>
               <p>with our top rated speakers</p>
             <button className='btn'>SHOP NOW</button>
             <div className='box'>
                   <span>50% OFF</span> on Rs.25000
-
             </div>
         </div>
+      {/* <div className="middle-section">
+          <h2>Current date and time</h2>
+        <p>{now.format("DD-MM-YYYY hh:mm:ss a")}</p>
+        </div>
+        */} 
         <div className="right-section">
            <img src={model} alt="" /> 
         </div>
