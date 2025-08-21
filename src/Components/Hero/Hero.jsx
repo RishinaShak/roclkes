dbd405b8fa988fa2807e33eed5d368b71eada07d
RishinaShak <117798295+RishinaShak@users.noreply.github.com>
2025-08-21 10:15:28 +0545

    Add files via upload

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Hero.css';
+import model from '../../assets/model1.png'
+
+const Hero = () => {
+  return (
+    <div className='container'>
+        <div className="left-section">
+              <p>Experience crystal </p>
+              <h1>clear sound</h1>
+              <p>with our top rated speakers</p>
+            <button className='btn'>SHOP NOW</button>
+            <div className='box'>
+                  <span>50% OFF</span> on Rs.25000
+            </div>
+        </div>
+        <div className="right-section">
+           <img src={model} alt="" /> 
+        </div>
+      
+    </div>
+  )
+}
+
+export default Hero
